00c52b49acc5cc91286c77bd3d0f15168a19952d
Jerzjon <jon.alv89@gmail.com>
2021-10-03 23:53:43 -0400

    changes to jss

--- Develop/script.js
+++ Develop/script.js
@@ -1,84 +1,109 @@
-// Assignment code here
-
 function getRandomPositonInArray(arrayLength) {
   
-    var randomNumber=Math.random()
-    var randomValue=randomNumber*arrayLength;
-    return Math.floor(randomValue);
+  var randomNumber=Math.random()
+  var randomValue=randomNumber*arrayLength;
+  return Math.floor(randomValue);
 
 }
+
+function generateRandomLowerCaseLetter(passLength) {
+var passwordArray = []
+
+for (var i = 0; i < passLength; i++) {
+    var randomPosition =getRandomPositionInArray(lowerCaseLetters.length);
+    var randomLetter = lowerCaseLetters[randomPosition]
+    passwordArray.push(randomLetter);
+
+}
+console.log(passwordArray.join(""))
+
+
+
+}
+
+
+function generatePassword() {
+var length = window.prompt("How many characters would you like to use?")
+console.log(length)
+generateRandomLowerCaseLetter(length)
+}
+generatePassword()
+
 
 function generateRandomUpperCaseLetter() {
-    
-    var upperCaseLetters=["A","B","C","D"];
-    var randomPosition=getRandomPositonInArray(upperCaseLetters.length);
-    return upperCaseLetters[randomPosition];
   
+  var upperCaseLetters=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  var randomPosition=getRandomPositonInArray(upperCaseLetters.length);
+  return upperCaseLetters[randomPosition];
+
 }
-  
+
 function generateRandomNumber(){
-    
-    var numbers=[0, 1, 2, 3, 4, 5,];
-    var randomPosition=getRandomPositonInArray(numbers.length);
-    return numbers[randomPosition];
+  
+  var numbers=[0, 1, 2, 3, 4, 5,];
+  var randomPosition=getRandomPositonInArray(numbers.length);
+  return numbers[randomPosition];
 
 }
 
 function generateRandomPassword() {
-    var includeSpecialCharacters=false;
-    var includerUpperCaseLetters=true;
-    var includeLowerCaseLetters=false;
-    var includeNumbers=true;
-    var passwordLength=10;
-    
-    var password="";
+  var includeSpecialCharacters=false;
+  var includerUpperCaseLetters=true;
+  var includeLowerCaseLetters=false;
+  var includeNumbers=true;
+  var passwordLength=10;
+  
+  var password="";
 
-    if(includeSpecialCharacters) {
-        password+=generateRandomSpecialCharacter();
-    }
-
-    if(includerUpperCaseLetters) {
-        password+=generateRandomUpperCaseLetter();
-    }
-    
-    if(includeLowerCaseLetters) {
-        password+=generateRandomLowerCaseLetter();
-    }
-
-    if(includeNumbers) {
-        password+=generateRandomNumber();
-    }
-
-    for(var i=password.length;i<passwordLength;i++) {
-        var typeOfCharacterToGenerate="upper"
-
-        if(typeOfCharacterToGenerate==="upper") {
-            password+=generateRandomUpperCaseLetter();
-        }
-    
+  if(includeSpecialCharacters) {
+      password+=generateRandomSpecialCharacter();
   }
-  return password;
-}
-console.log(generateRandomPassword())
-console.log(generateRandomPassword())
-console.log(generateRandomPassword())
-console.log(generateRandomPassword())
-console.log(generateRandomPassword())
 
+  if(includerUpperCaseLetters) {
+      password+=generateRandomUpperCaseLetter();
+  }
+  
+  if(includeLowerCaseLetters) {
+      password+=generateRandomLowerCaseLetter();
+  }
 
+  if(includeNumbers) {
+      password+=generateRandomNumber();
+  }
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+  for(var i=password.length;i<passwordLength;i++) {
+      var typeOfCharacterToGenerate="upper"
 
-// Write password to the #password input
+      if(typeOfCharacterToGenerate==="upper") {
+          password+=generateRandomUpperCaseLetter();
+      }
+
+function getRandom(array) {
+  var randomIndex = Math.floor(Math.random() * array.length);
+  }
+
+// write password to the password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-}
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-generateBtn.addEventListener("click", pwdPrompt); 
+
+}      
+
+
+
+
+
+
+
+}
+return password;
+}
+console.log(generateRandomPassword())
+console.log(generateRandomPassword())
+console.log(generateRandomPassword())
+console.log(generateRandomPassword())
+console.log(generateRandomPassword())
