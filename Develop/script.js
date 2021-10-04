ed35d69c9f511de8462fc1f73e67027d86a42054
Jerzjon <jon.alv89@gmail.com>
2021-10-04 18:00:47 -0400

    added additional arrays and functions, pw gen button

--- Develop/script.js
+++ Develop/script.js
@@ -1,43 +1,7 @@
 var generateBtn = document.querySelector("#generate");
 
-// Main password function
-function generatePassword() {
-  var passwordLengthConf = prompt("Choose a password length: 8-128 characters.");
-
-// Password length selector by user input
-    
-  if (passwordLengthConf < 8 || passwordLengthConf > 128) {
-      alert("Please enter a number between 8-128.");
-      passwordLengthConf = 0; 
-      return generatePassword();
-
-};
-
-// Confirm Prompts
-var selectLowercase = confirm("Would you like to include lowercase letters?");
-var selectUppercase = confirm("Would you like to include uppercase letters?");
-var selectNumberChar = confirm("Would you like to include numbers?")
-var selectSpecialChar = confirm("Would you like to include special characters?");
-
-// Console loggin the user input
-console.log(passwordLengthConf);
-console.log(selectLowercase);
-console.log(selectUppercase);
-console.log(selectNumberChar);
-console.log(selectSpecialChar);
-
-// Variables for user input
-function generateRandomPassword() {
-  var includeSpecialChar=selectSpecialChar;
-  var includeUppercaseLetters=selectUppercase;
-  var includeLowercaseLetters=selectLowercase;
-  var includeNumberChar=selectNumberChar;
-  var passwordLength=passwordLengthConf;
-  
-  var password="";
-
 // Character randomizer
-function getRandomPositonInArray(arrayLength) {
+function getRandomPositionInArray(arrayLength) {
   
   var randomNumber=Math.random()
   var randomValue=randomNumber*arrayLength;
@@ -46,27 +10,27 @@ function getRandomPositonInArray(arrayLength) {
 };
 
 // Arrays for password characters
-function generateRandomUpperCaseLetter() {
+function generateRandomUppercaseLetter() {
   
-  var selectUppercase=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-  var randomPosition=getRandomPositonInArray(selectUppercase.length);
-  return selectUppercase[randomPosition];
+  var uppercaseLetters=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  var randomPosition=getRandomPositionInArray(uppercaseLetters.length);
+  return uppercaseLetters[randomPosition];
 
 };
 
 function generateRandomNumberChar() {
     
-  var selectNumberChar=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  var randomPosition=getRandomPositionInArray(selectNumberChar.length);
-  return selectNumberChar[randomPosition];
+  var numberChar=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var randomPosition=getRandomPositionInArray(numberChar.length);
+  return numberChar[randomPosition];
   
 };
 
-function generateRandomLowerCaseLetter() {
+function generateRandomLowercaseLetter() {
 
-  var selectLowercase=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-  var randomPosition=getRandomPositonInArray(selectLowercase.length);
-  return selectLowercase[randomPosition];
+  var lowercaseLetters=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  var randomPosition=getRandomPositionInArray(lowercaseLetters.length);
+  return lowercaseLetters[randomPosition];
 
 };
 
@@ -74,7 +38,7 @@ function generateRandomLowerCaseLetter() {
 function generateRandomSpecialChar() {
 
   var selectSpecialChar=['/','[','!','@','#','$','%','^','&','*','(',')','_','+','-','=','[',']','{','}',';',':','"','|',',','.','<','>','?',']','+'];
-  var randomPosition=getRandomPositonInArray(selectSpecialChar.length);
+  var randomPosition=getRandomPositionInArray(selectSpecialChar.length);
   return selectSpecialChar[randomPosition];
 
 };
@@ -128,79 +92,118 @@ function generateRandomSpecialCharAndUppercase() {
   
 };
 
+// Main password function
+function generatePassword() {
+  var passwordLengthConf = prompt("Choose a password length: 8-128 characters.");
+
+// Password length selector by user input
+    
+  if (passwordLengthConf < 8 || passwordLengthConf > 128) {
+      alert("Please enter a number between 8-128.");
+      passwordLengthConf = 0; 
+      return generatePassword();
+
+};
+
+// Confirm Prompts
+var selectLowercase = confirm("Would you like to include lowercase letters?");
+var selectUppercase = confirm("Would you like to include uppercase letters?");
+var selectNumberChar = confirm("Would you like to include numbers?")
+var selectSpecialChar = confirm("Would you like to include special characters?");
+
+// Console loggin the user input
+console.log(passwordLengthConf);
+console.log(selectLowercase);
+console.log(selectUppercase);
+console.log(selectNumberChar);
+console.log(selectSpecialChar);
+
+// Variables for user input
+  
+  var includeSpecialChar=selectSpecialChar;
+  var includeUppercaseLetters=selectUppercase;
+  var includeLowercaseLetters=selectLowercase;
+  var includeNumberChar=selectNumberChar;
+  var passwordLength=passwordLengthConf;
+  
+  var password="";
+
 // If/Else statements for user input
 
-  if(includeSpecialChar) {
-    password+=generateRandomSpecialChar();
-  }
-  
-  if(includeUppercaseLetters) {
-    password+=generateRandomUppercaseLetter();
-  } 
-  
-  if(includeLowercaseLetters) {
-    password+=generateRandomLowercaseLetter();
-  }
-  
-  if(includeNumberChar) {
-    password+=generateRandomNumberChar();
-  }
+if(includeSpecialChar) {
+  password+=generateRandomSpecialChar();
+}
 
-  // Loop determining password length
-  for(var i = password.length; i < passwordLength; i++){
-      var typeOfCharacterToGenerate=true
-
-      // Possible parameters
-      if(typeOfCharacterToGenerate===selectSpecialChar && typeOfCharacterToGenerate===selectNumberChar && typeOfCharacterToGenerate===selectUppercase && typeOfCharacterToGenerate===selectLowercase){
-        password+=generateRandomAll();
-      }
-      else if(typeOfCharacterToGenerate===selectSpecialChar && typeOfCharacterToGenerate===selectNumberChar && typeOfCharacterToGenerate===selectUppercase){
-        password+=generateRandomSpecialNumberUpper();
-      }
-      else if(typeOfCharacterToGenerate===selectSpecialChar && typeOfCharacterToGenerate===selectNumberChar && typeOfCharacterToGenerate===selectLowercase){
-        password+=generateRandomSpecialNumberLower();
-      }
-      else if(typeOfCharacterToGenerate===selectSpecialChar && typeOfCharacterToGenerate===selectUppercase){
-        password+=generateRandomSpecialCharAndUppercase();
-      }
-      else if(typeOfCharacterToGenerate===selectSpecialChar && typeOfCharacterToGenerate===selectLowercase){
-        password+=generateRandomSpecialCharAndUppercase();
-      }
-      else if(typeOfCharacterToGenerate===selectSpecialChar && typeOfCharacterToGenerate===selectNumberChar){
-        password+=generateRandomSpecialCharAndUppercase();
-      }
-      else if(typeOfCharacterToGenerate===selectNumberChar && typeOfCharacterToGenerate===selectUppercase){
-        password+=generateRandomNumberUpper();
-      }
-      else if(typeOfCharacterToGenerate===selectNumberChar && typeOfCharacterToGenerate===selectLowercase){
-        password+=generateRandomNumberLower();
-      }
-      else if(typeOfCharacterToGenerate===selectUppercase){
-        password+=generateRandomUppercaseLetter();
-      }
-      else if(typeOfCharacterToGenerate===selectLowercase){
-        password+=generateRandomLowerCaseLetter();
-      }
-      else if(typeOfCharacterToGenerate===selectNumberChar){
-        password+=generateRandomNumberChar();
-      }
-      else if(typeOfCharacterToGenerate===selectSpecialChar){
-        password+=generateRandomSpecialChar();
-      }
-  }
-  
-  return password;
-
+if(includeUppercaseLetters) {
+  password+=generateRandomUppercaseLetter();
 } 
+
+if(includeLowercaseLetters) {
+  password+=generateRandomLowercaseLetter();
+}
+
+if(includeNumberChar) {
+  password+=generateRandomNumberChar();
+}
+
+// Loop determining password length
+    for(var i = password.length; i < passwordLength; i++){
+    var typeOfCharacterToGenerate=true
+
+    // Possible parameters
+    if(typeOfCharacterToGenerate===selectSpecialChar && typeOfCharacterToGenerate===selectNumberChar && typeOfCharacterToGenerate===selectUppercase && typeOfCharacterToGenerate===selectLowercase){
+      password+=generateRandomAll();
+    }
+    else if(typeOfCharacterToGenerate===selectSpecialChar && typeOfCharacterToGenerate===selectNumberChar && typeOfCharacterToGenerate===selectUppercase){
+      password+=generateRandomSpecialNumberUpper();
+    }
+    else if(typeOfCharacterToGenerate===selectSpecialChar && typeOfCharacterToGenerate===selectNumberChar && typeOfCharacterToGenerate===selectLowercase){
+      password+=generateRandomSpecialNumberLower();
+    }
+    else if(typeOfCharacterToGenerate===selectSpecialChar && typeOfCharacterToGenerate===selectUppercase){
+      password+=generateRandomSpecialCharAndUppercase();
+    }
+    else if(typeOfCharacterToGenerate===selectSpecialChar && typeOfCharacterToGenerate===selectLowercase){
+      password+=generateRandomSpecialCharAndUppercase();
+    }
+    else if(typeOfCharacterToGenerate===selectSpecialChar && typeOfCharacterToGenerate===selectNumberChar){
+      password+=generateRandomSpecialCharAndUppercase();
+    }
+    else if(typeOfCharacterToGenerate===selectNumberChar && typeOfCharacterToGenerate===selectUppercase){
+      password+=generateRandomNumberUpper();
+    }
+    else if(typeOfCharacterToGenerate===selectNumberChar && typeOfCharacterToGenerate===selectLowercase){
+      password+=generateRandomNumberLower();
+    }
+    else if(typeOfCharacterToGenerate===selectUppercase){
+      password+=generateRandomUpperCaseLetter();
+    }
+    else if(typeOfCharacterToGenerate===selectLowercase){
+      password+=generateRandomLowerCaseLetter();
+    }
+    else if(typeOfCharacterToGenerate===selectNumberChar){
+      password+=generateRandomNumberChar();
+    }
+    else if(typeOfCharacterToGenerate===selectSpecialChar){
+      password+=generateRandomSpecialChar();
+    }
+}
+
+return password;
+
+
+};
+
+
 
   // Write password to the #password input
   function writePassword() {
-    var password = generateRandomPassword();
+    var password = generatePassword();
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
 
   }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
